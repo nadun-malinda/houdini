d7deb3a57116d02522a31b73e977003d89ca3c53
nadun-malinda <bknandun@gmail.com>
2023-06-20 10:40:28 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "css-paint-polyfill";
+
+//@ts-ignore
+// CSS.paintWorklet.addModule("./worklets/curved-line.js");
+
+//@ts-ignore
+CSS.paintWorklet.addModule(
+  "https://unpkg.com/curved-line@1.0.0/curved-line.js"
+);
 
 function App() {
   return (
@@ -10,6 +19,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <hr />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ types/css-paint-polyfill.d.ts
@@ -0,0 +1,3 @@
+declare module "css-paint-polyfill" {
+  export const registerPaint: (name: string, paintCtor: Function) => void;
+}
